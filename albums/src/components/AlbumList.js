e231eb4941f9f23c5d70453304fad88c0c0ce8d7
highperch <jpeng@berkeley.edu>
2017-05-30 17:38:41 -0700

    Pull and display album details
    Created AlbumDetails component, which AlbumList uses to display individual albums.

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,15 +1,34 @@
 // Import libraries
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 // Create the component
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>Album List!!!</Text>
-    </View>
-  );
-};
+class AlbumList extends Component {
+  state = { albums: [] };
+
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album} />
+    );
+  }
+
+  render() {
+    console.log(this.state);
+
+    return (
+      <View>
+        {this.renderAlbums()}
+      </View>
+    );
+  }
+}
 
 // Export the component
-export default AlbumList
+export default AlbumList;
